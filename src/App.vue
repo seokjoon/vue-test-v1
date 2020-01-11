08f312de79cae71992307ef1007855aadf857a6a
seokjoon <seokjoon@gmail.com>
2020-01-11 21:31:00 +0900

    public test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <T1 item="test" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import T1 from "./components/T1";
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    T1,
+    // HelloWorld
+  },
 }
 </script>
 
---
+++ src/components/T1.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		{{ item }}
+	</div>
+</template>
+
+
+<script>
+	export default {
+		name: 'T1',
+		props: {
+			item: String,
+		},
+	};
+	console.log('T1');
+</script>
