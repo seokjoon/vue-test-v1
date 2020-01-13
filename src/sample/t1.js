47133f3cd8aba2e391e2b225ada67d12d4910650
seokjoon <seokjoon@gmail.com>
2020-01-13 22:42:43 +0900

    component

--- src/sample/t1.js
+++ src/sample/t1.js
@@ -18,9 +18,27 @@ Vue.component('component3', component3);
 new Vue({ el:'#component', });
 
 new Vue({
+	el: '#componentLocal',
+	template: '<span><component-local1></component-local1><component-local2></component-local2></span>',
+	components: {
+		componentLocal1: { template: '<span>componentLocal1</span>', },
+		componentLocal2: component3,
+	},
+});
+
+new Vue({
 	computed: { compute1: function() { return this.items[0] + this.items[1] }, },
 	data: { items: [ 2, 3, ], },
 	el: '#compute',
+});
+
+Vue.component('componentProps1', {
+	props: { item: { type: String, default: 'default', required: false, validator: () => true, }, },
+	template: '<span>{{ item }}</span>',
+});
+new Vue({
+	data: { item: 'item props' },
+	el: '#componentProps',
 });
 
 new Vue({
@@ -46,6 +64,13 @@ new Vue({
 	el: '#method',
 	methods: { method1: function($e) { console.log($e.target.name); }, },
 });
+
+Vue.component('templateX1', { template: '#templateX1', });
+new Vue({
+	el: '#templateX2',
+	template: '<template-x1></template-x1>',
+});
+
 
 new Vue({
 	data: { items: [{title: 'bar'}], },
