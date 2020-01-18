c74804eacf0f6cc62462697dac787609cffda3cb
seokjoon <seokjoon@gmail.com>
2020-01-18 14:56:30 +0900

    vue spa

--- src/sample/t2.js
+++ src/sample/t2.js
@@ -1,11 +1,31 @@
 if(typeof Vue === 'undefined') var Vue = () => {};
 
 
-var getUsers = function(cb) {
-	setTimeout(function() { cb(null, [ { id: 1, name: 'foo' }, { id: 2, name: 'bar', } ]); }, 1000);
+var userData = [ { id: 1, name: 'foo', desc: 'foo desc', }, { id: 2, name: 'bar', desc: 'bar desc', } ];
+var getUser = function (id, cb) {
+	setTimeout(function() {
+		var filteredUsers = userData.filter(function(user) { return user.id === parseInt(id, 10); });
+		cb(null, filteredUsers && filteredUsers[0]);
+	}, 500);
 };
-//var UserList = { template: '#spa-user-list', };
-Vue.component('UserList', {
+var UserItem = {
+	created: function() { this.fetchData(); },
+	data: () => ({ loading: false, user: null, error: null }),
+	methods: {
+		fetchData: function() {
+			this.loading = true;
+			getUser(this.$route.params.id, (function(err, user) {
+				this.loading = false;
+				if(err) this.error = err.toString();
+				else this.user = user;
+			}).bind(this));
+		},
+	},
+	template: '#spa-user-item',
+	watch: { '$route': 'fetchData', },
+};
+var getUsers = function(cb) { setTimeout(function() { cb(null, userData); }, 500); };
+var UserList = {
 	created: function() { this.fetchData(); },
 	data: function() { return { loading: false, users: () => [], error: null, }; },
 	methods: {
@@ -20,11 +40,14 @@ Vue.component('UserList', {
 	},
 	template: '#spa-user-list',
 	watch: { '$route': 'fetchData', },
-});
+};
 var routerSpa = new VueRouter({
 	routes: [
 		{ path: '/top', component: { template: '<div>top</div>', }, },
-		{ path: '/users', component: { template: '<span><user-list></user-list></span>', }, },
+		//{ path: '/users', component: { template: '<span><user-list></user-list></span>', }, },
+		{ path: '/users', component: UserList, },
+		//{ path: '/users/:id', component: '<span><user-item></user-item></span>', }
+		{ path: '/users/:id', component: UserItem, }
 	],
 });
 var spa = new Vue({ router: routerSpa, });
