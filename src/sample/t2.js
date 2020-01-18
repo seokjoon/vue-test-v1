4229b10ef2b39c20238f8d8848c366424ffa2090
seokjoon <seokjoon@gmail.com>
2020-01-18 13:50:27 +0900

    vue spa

--- src/sample/t2.js
+++ src/sample/t2.js
@@ -1,5 +1,36 @@
 if(typeof Vue === 'undefined') var Vue = () => {};
 
+
+var getUsers = function(cb) {
+	setTimeout(function() { cb(null, [ { id: 1, name: 'foo' }, { id: 2, name: 'bar', } ]); }, 1000);
+};
+//var UserList = { template: '#spa-user-list', };
+Vue.component('UserList', {
+	created: function() { this.fetchData(); },
+	data: function() { return { loading: false, users: () => [], error: null, }; },
+	methods: {
+		fetchData: function() {
+			this.loading = true;
+			getUsers((function(err, users) {
+				this.loading = false;
+				if(err) this.error = err.toString();
+				else this.users = users;
+			}).bind(this));
+		},
+	},
+	template: '#spa-user-list',
+	watch: { '$route': 'fetchData', },
+});
+var routerSpa = new VueRouter({
+	routes: [
+		{ path: '/top', component: { template: '<div>top</div>', }, },
+		{ path: '/users', component: { template: '<span><user-list></user-list></span>', }, },
+	],
+});
+var spa = new Vue({ router: routerSpa, });
+spa.$mount('#spa');
+
+
 var router = new VueRouter({
 	routes: [
 		{ path: '/link1', component: { template: '<div>link1</div>' }, },
