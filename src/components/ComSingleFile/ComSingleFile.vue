7124757098f7691b69dd06d6fe86be0f751d78e8
seokjoon <seokjoon@gmail.com>
2020-01-27 13:20:10 +0900

    com single file

---
+++ src/components/ComSingleFile/ComSingleFile.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<p class="message">메시지: {{ msg }}</p>
+		<p :class="$style.message">메시지: {{ msg }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data: () => ({ msg: 'msg comSingleFile' }),
+	};
+</script>
+
+<style src="./ComSingleFile.css"></style> <!-- 외부 -->
+<style> .message { color: #1e8ad6 } </style> <!-- 전역 -->
+<style scoped></style> <!-- 지역 -->
+<style module> .message { color: #42b983 } </style> <!-- 모듈 -->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+//import App from './App.vue';
+import ComSingleFile from './components/ComSingleFile/ComSingleFile';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//const app = App;
+const app = ComSingleFile;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(app),
+}).$mount('#app');
